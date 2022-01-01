5026ffeb08c134c7b0a0200652e3891e202e192e
Christopher Dent <christopherdent01@gmail.com>
2022-01-01 17:28:57 -0500

    practicing input

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-msg = "Hello, World"
-age = 24
+##msg = "Hello, World"
+# age = 20
+# first_name = 'John Smith'
+# new_patient = True 
 
-print(msg)
+# print(first_name)
+# print(age)
+# print(new_patient)
  
 
+name = input("What's your name? ")
+
+print("Hello " + name + "!")
